95c6885909a8acb91136851c8de02c272f8c1721
Pablo Goulart <pablogoulart02@gmail.com>
2024-01-10 21:44:14 -0300

    Site montado via tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Manrope: ['Manrope', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
