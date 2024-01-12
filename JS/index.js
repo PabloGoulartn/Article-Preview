36d98fbf6732ec7f059a6e771e41fb8c6579c441
Pablo Goulart <pablogoulart02@gmail.com>
2024-01-12 20:14:55 -0300

    funcionalidade javascript implementada

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,20 @@
-document.querySelector('.btn-share').addEventListener('click', function(){
-    alert('teste');
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const getBtnShare = document.querySelectorAll('.share');
+
+    getBtnShare.forEach(function (btnShare) {
+        btnShare.style.opacity = "0";
+    });
+
+    document.querySelector('.share-icon').addEventListener('mouseenter', function () {
+        getBtnShare.forEach(function (btnShare) {
+            btnShare.style.opacity = "1";
+        });
+    });
+
+    document.querySelector('.share-icon').addEventListener('mouseleave', function () {
+        getBtnShare.forEach(function (btnShare) {
+            btnShare.style.opacity = "0";
+        });
+    });
+
+})
