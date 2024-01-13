8408a70cb10647807db299fa25c37ebb88d39a55
Pablo Goulart <pablogoulart02@gmail.com>
2024-01-13 14:42:21 -0300

    melhoria no codigo javascript

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
+function setOpacity(opacityNumber){
     const getBtnShare = document.querySelectorAll('.share');
 
     getBtnShare.forEach(function (btnShare) {
-        btnShare.style.opacity = "0";
+        btnShare.style.opacity = opacityNumber;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    setOpacity(0);
+
+    const shareIcon = document.querySelector('.share-icon');
+
+    shareIcon.addEventListener('mouseenter', function () {
+        setOpacity(1);
     });
 
-    document.querySelector('.share-icon').addEventListener('mouseenter', function () {
-        getBtnShare.forEach(function (btnShare) {
-            btnShare.style.opacity = "1";
-        });
+    shareIcon.addEventListener('mouseleave', function () {
+        setOpacity(0);
     });
-
-    document.querySelector('.share-icon').addEventListener('mouseleave', function () {
-        getBtnShare.forEach(function (btnShare) {
-            btnShare.style.opacity = "0";
-        });
-    });
-
 })
